297391b206e4399efabd5840c556d14d028b4334
Vincent Yao <viyao@paypal.com>
2020-12-06 15:32:25 +0800

    dashboard

---
+++ platform/hystrix-dashboard/src/main/java/com/imooc/platform/DashboardApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}

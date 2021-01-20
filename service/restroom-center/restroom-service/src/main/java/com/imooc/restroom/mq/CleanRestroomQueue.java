96c8728d02e0ca0baf3d2d85c69e365627eb1637
Vincent Yao <viyao@paypal.com>
2021-01-20 22:10:17 +0800

    stream

--- service/restroom-center/restroom-service/src/main/java/com/imooc/restroom/mq/CleanRestroomQueue.java
+++ service/restroom-center/restroom-service/src/main/java/com/imooc/restroom/mq/CleanRestroomQueue.java
@@ -1,15 +1,15 @@
-package com.broadview.coupon.user.mq;
+package com.imooc.restroom.mq;
 
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
-public interface RequestCouponQueue {
+public interface CleanRestroomQueue {
 
-    String INPUT = "coupon-consumer";
+    String INPUT = "rm-consumer";
 
-    String OUTPUT = "coupon-producer";
+    String OUTPUT = "rm-producer";
 
     @Input(INPUT)
     public SubscribableChannel input();
